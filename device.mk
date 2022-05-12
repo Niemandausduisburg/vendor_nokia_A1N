555b316f81fb1679cab28a6dc7e889df157f48e2
Kevin Wilms <niemandausduisburg@gmx.net>
2022-05-12 20:13:26 +0200

    A1N: Build libkeymaster_messages vendor variant

--- device.mk
+++ device.mk
@@ -204,8 +204,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.stats@1.0.vendor \
     android.hardware.biometrics.fingerprint@2.1.vendor \
-    android.hardware.biometrics.fingerprint@2.2.vendor \
-    android.hardware.biometrics.fingerprint@2.1-service 
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libkeymaster_messages.vendor
 
 # FM
 PRODUCT_PACKAGES += \
